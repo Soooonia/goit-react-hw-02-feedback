46e523e080cd5af8c0f02d9aa40b9319192c946c
Soooonia <121182239+Soooonia@users.noreply.github.com>
2023-07-11 14:41:34 +0300

    &

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+// import React from 'react';
+// // import ReactDOM from 'react-dom/client';
+// import { App } from 'components/App';
+// import './index.css';
+
+// const container = document.querySelector('#root');
+// const root = createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom/client';
+import App from 'components/App';
 import './index.css';
-
-const container = document.querySelector('#root');
-const root = createRoot(container);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
